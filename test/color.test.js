c6a82e527488584b17f90e88680995e0b87a4d14
Florens Verschelde <243601+fvsch@users.noreply.github.com>
2024-09-12 11:28:57 +0200

    fix: add mechanism to force enable colors for tests

--- test/color.test.js
+++ test/color.test.js
@@ -1,44 +1,60 @@
 import { strictEqual } from 'node:assert';
 import { suite, test } from 'node:test';
 
-import { brackets, seqStyle, stripStyle, style } from '../lib/color.js';
+import { ColorUtils } from '../lib/color.js';
 
-suite('style', () => {
+suite('ColorUtils.style', () => {
 	test('does nothing for empty format', () => {
+		const { style } = new ColorUtils({ enabled: true });
 		strictEqual(style('TEST'), 'TEST');
 		strictEqual(style('TEST', ''), 'TEST');
 	});
 
-	// TODO: Results depend on terminal, so this test will probably fail
-	// in different settings or CI.
 	test('adds color codes to strings', () => {
-		strictEqual(style('TEST', 'reset', true), '\x1B[0mTEST\x1B[0m');
-		strictEqual(style('TEST', 'red', true), '\x1B[31mTEST\x1B[39m');
-		strictEqual(style('TEST', 'dim underline', true), '\x1B[2m\x1B[4mTEST\x1B[24m\x1B[22m');
+		const { style } = new ColorUtils({ enabled: true });
+		strictEqual(style('TEST', 'reset'), '\x1B[0mTEST\x1B[0m');
+		strictEqual(style('TEST', 'red'), '\x1B[31mTEST\x1B[39m');
+		strictEqual(style('TEST', 'dim underline'), '\x1B[2m\x1B[4mTEST\x1B[24m\x1B[22m');
+	});
+
+	test('does nothing when colors are disabled', () => {
+		const { style } = new ColorUtils({ enabled: false });
+		strictEqual(style('TEST', 'reset'), 'TEST');
+		strictEqual(style('TEST', 'red'), 'TEST');
+		strictEqual(style('TEST', 'dim underline'), 'TEST');
 	});
 });
 
-suite('seqStyle', () => {
+suite('ColorUtils.sequence', () => {
 	test('applies styles to sequence', () => {
-		strictEqual(seqStyle(['TE', 'ST']), 'TEST');
-		strictEqual(seqStyle(['(', 'TEST', ')']), '(TEST)');
-		strictEqual(seqStyle(['TE', 'ST'], 'blue'), '\x1B[34mTE\x1B[39mST');
-		strictEqual(seqStyle(['TE', 'ST'], ',blue'), 'TE\x1B[34mST\x1B[39m');
-		strictEqual(seqStyle(['TE', 'ST'], 'blue,red,green'), '\x1B[34mTE\x1B[39m\x1B[31mST\x1B[39m');
+		const { sequence } = new ColorUtils({ enabled: true });
+		strictEqual(sequence(['TE', 'ST']), 'TEST');
+		strictEqual(sequence(['(', 'TEST', ')']), '(TEST)');
+		strictEqual(sequence(['TE', 'ST'], 'blue'), '\x1B[34mTE\x1B[39mST');
+		strictEqual(sequence(['TE', 'ST'], ',blue'), 'TE\x1B[34mST\x1B[39m');
+		strictEqual(sequence(['TE', 'ST'], 'blue,red,green'), '\x1B[34mTE\x1B[39m\x1B[31mST\x1B[39m');
 	});
 });
 
-suite('stripStyle', () => {
+suite('ColorUtils.strip', () => {
 	test('formatting can be removed', () => {
-		strictEqual(stripStyle(style('TEST', 'magentaBright')), 'TEST');
-		strictEqual(stripStyle(seqStyle(['T', 'E', 'S', 'T'], 'inverse,blink,bold,red')), 'TEST');
+		const { style, sequence, strip } = new ColorUtils({ enabled: true });
+		strictEqual(strip(style('TEST', 'magentaBright')), 'TEST');
+		strictEqual(strip(sequence(['T', 'E', 'S', 'T'], 'inverse,blink,bold,red')), 'TEST');
 	});
 });
 
-suite('brackets', () => {
+suite('ColorUtils.brackets', () => {
 	test('adds brackets around input', () => {
+		const { brackets } = new ColorUtils({ enabled: true });
 		strictEqual(brackets('TEST', ''), '[TEST]');
 		strictEqual(brackets('TEST'), '\x1B[2m[\x1B[22mTEST\x1B[2m]\x1B[22m');
 		strictEqual(brackets('TEST', 'blue,,red'), '\x1B[34m[\x1B[39mTEST\x1B[31m]\x1B[39m');
+	});
+
+	test('supports custom brackers', () => {
+		const { brackets } = new ColorUtils({ enabled: true, brackets: ['<<<', '>>>'] });
+		strictEqual(brackets('TEST', ''), '<<<TEST>>>');
+		strictEqual(brackets('TEST', 'blue,,red'), '\x1B[34m<<<\x1B[39mTEST\x1B[31m>>>\x1B[39m');
 	});
 });
