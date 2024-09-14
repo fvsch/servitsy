189f269c26c97829066f61b35985e11bc717fd5f
Florens Verschelde <243601+fvsch@users.noreply.github.com>
2024-09-15 01:14:53 +0200

    feat: get content-type from file bytes

--- test/server.test.js
+++ test/server.test.js
@@ -5,69 +5,94 @@ import { fileHeaders } from '../lib/server.js';
 
 /**
 @typedef {import('../lib/types.js').HttpHeaderRule} HttpHeaderRule
-@typedef {{ cors: boolean; headers: HttpHeaderRule[] }} FileHeadersOptions
+@typedef {import('../lib/types.js').ServerOptions} ServerOptions
+@typedef {Parameters<typeof fileHeaders>[0]} FileHeadersData
 **/
 
 suite('fileHeaders', () => {
-	/** @type {FileHeadersOptions} */
-	const baseOptions = { cors: false, headers: [] };
-
-	/** @type {(baseOptions: FileHeadersOptions) => (filePath: string, options?: Partial<FileHeadersOptions>) => Record<string, string>} */
-	const headersWith = (baseOptions) => {
-		return (filePath, options = {}) => fileHeaders(filePath, { ...baseOptions, ...options });
+	/**
+	 * @param {Partial<FileHeadersData> & { localPath: string }} data
+	 * @param {Record<string, string>} expected
+	 */
+	const checkHeaders = (data, expected) => {
+		deepStrictEqual(fileHeaders({ cors: false, headers: [], ...data }), expected);
 	};
 
+	test('keeps provided content-type header', () => {
+		checkHeaders(
+			{ localPath: 'foo', contentType: 'text/html' },
+			{
+				'content-type': 'text/html',
+			},
+		);
+	});
+
 	test('sets content-type header', () => {
-		const headersFor = headersWith(baseOptions);
-		deepStrictEqual(headersFor('foo'), {
-			'content-type': 'application/octet-stream',
-		});
-		deepStrictEqual(fileHeaders('hello.html', baseOptions), {
-			'content-type': 'text/html; charset=UTF-8',
-		});
+		checkHeaders(
+			{ localPath: 'foo' },
+			{
+				'content-type': 'application/octet-stream',
+			},
+		);
+		checkHeaders(
+			{ localPath: 'hello.html' },
+			{
+				'content-type': 'text/html; charset=UTF-8',
+			},
+		);
 	});
 
 	test('cors option sets access-control-allow-origin', () => {
-		const headersFor = headersWith(baseOptions);
-		deepStrictEqual(headersFor('foo', { cors: false }), {
-			'content-type': 'application/octet-stream',
-		});
-		deepStrictEqual(headersFor('foo', { cors: true }), {
-			'content-type': 'application/octet-stream',
-			'access-control-allow-origin': '*',
-		});
+		checkHeaders(
+			{ localPath: 'foo', cors: false },
+			{
+				'content-type': 'application/octet-stream',
+			},
+		);
+		checkHeaders(
+			{ localPath: 'foo', cors: true },
+			{
+				'content-type': 'application/octet-stream',
+				'access-control-allow-origin': '*',
+			},
+		);
 	});
 
 	test('headers without include patterns are added for all responses', () => {
-		const headersFor = headersWith({
-			cors: false,
-			headers: [
-				{ headers: { 'X-Header1': 'one' } },
-				{ headers: { 'X-Header2': 'two' } },
-				{ headers: { 'x-header1': 'three' } },
-			],
-		});
-		deepStrictEqual(headersFor('some/file.txt'), {
-			'content-type': 'text/plain; charset=UTF-8',
-			'x-header1': 'three',
-			'x-header2': 'two',
-		});
+		/** @type {FileHeadersData['headers']} */
+		const headers = [
+			{ headers: { 'X-Header1': 'one' } },
+			{ headers: { 'X-Header2': 'two' } },
+			{ headers: { 'x-header1': 'three' } },
+		];
+		checkHeaders(
+			{ localPath: 'some/file.txt', headers },
+			{
+				'content-type': 'text/plain; charset=UTF-8',
+				'x-header1': 'three',
+				'x-header2': 'two',
+			},
+		);
 	});
 
 	test('custom headers with pattern are added matching files only', () => {
-		const headersFor = headersWith({
-			cors: false,
-			headers: [
-				{ include: ['path'], headers: { 'x-header1': 'true' } },
-				{ include: ['*.test'], headers: { 'Content-Type': 'test/custom-type' } },
-			],
-		});
-		deepStrictEqual(headersFor('README.test'), {
-			'content-type': 'test/custom-type',
-		});
-		deepStrictEqual(headersFor('path/to/README.md'), {
-			'content-type': 'text/markdown; charset=UTF-8',
-			'x-header1': 'true',
-		});
+		/** @type {FileHeadersData['headers']} */
+		const headers = [
+			{ include: ['path'], headers: { 'x-header1': 'true' } },
+			{ include: ['*.test'], headers: { 'Content-Type': 'test/custom-type' } },
+		];
+		checkHeaders(
+			{ localPath: 'README.test', headers },
+			{
+				'content-type': 'test/custom-type',
+			},
+		);
+		checkHeaders(
+			{ localPath: 'path/to/README.md', headers },
+			{
+				'content-type': 'text/markdown; charset=UTF-8',
+				'x-header1': 'true',
+			},
+		);
 	});
 });
